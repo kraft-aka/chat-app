0c8f964d274859daa0fa86fbcf8b7849227e982e
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-07-28 09:59:12 +0200

    comment out code

--- components/Chat.js
+++ components/Chat.js
@@ -8,166 +8,166 @@ import {
   Text
 } from "react-native";
 
-// import firebase
-import firebase from "firebase";
-import 'firebase/firestore';
+// // import firebase
+// import firebase from "firebase";
+// import 'firebase/firestore';
 
-export default function Chat(props) {
-  // state for holding messages
-  const [messages, setMessages] = useState([]);
-  const [ uid, setUid ] = useState(null);
-  const [ loggedText, setLoggedText ] = useState('Please wait. You are being authenticated')
+// export default function Chat(props) {
+//   // state for holding messages
+//   const [messages, setMessages] = useState([]);
+//   const [ uid, setUid ] = useState(null);
+//   const [ loggedText, setLoggedText ] = useState('Please wait. You are being authenticated')
 
-  // configuration of Firebase credentials
-  const firebaseConfig = {
-    apiKey: "AIzaSyDShzCnlD3dY0YlGmueSPVLIW1bIhCiDlE",
-    authDomain: "chatapp-ae72c.firebaseapp.com",
-    projectId: "chatapp-ae72c",
-    storageBucket: "chatapp-ae72c.appspot.com",
-    messagingSenderId: "850257656773",
-  };
+//   // configuration of Firebase credentials
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyDShzCnlD3dY0YlGmueSPVLIW1bIhCiDlE",
+//     authDomain: "chatapp-ae72c.firebaseapp.com",
+//     projectId: "chatapp-ae72c",
+//     storageBucket: "chatapp-ae72c.appspot.com",
+//     messagingSenderId: "850257656773",
+//   };
 
-  // set the Firebase 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+//   // set the Firebase 
+//   if (!firebase.apps.length) {
+//     firebase.initializeApp(firebaseConfig);
+//   }
 
-  //making reference to collection to store and retrieve the users messages
-  let referenceMessages = firebase.firestore().collection('messages');
+//   //making reference to collection to store and retrieve the users messages
+//   let referenceMessages = firebase.firestore().collection('messages');
 
-  // destructuring the props
-  let { name, color } = props.route.params;
+//   // destructuring the props
+//   let { name, color } = props.route.params;
 
-  // change title once user proceeds to chat
-  useEffect(() => {
-    props.navigation.setOptions({ title: name === "" ? "no title" : name });
-  }, []);
+//   // change title once user proceeds to chat
+//   useEffect(() => {
+//     props.navigation.setOptions({ title: name === "" ? "no title" : name });
+//   }, []);
 
-  let unsubscribe; 
+//   let unsubscribe; 
 
-  // update a state
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: `Hello ${name}`,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: "This is a system message",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
-  }, []);
+//   // update a state
+//   useEffect(() => {
+//     setMessages([
+//       {
+//         _id: 1,
+//         text: `Hello ${name}`,
+//         createdAt: new Date(),
+//         user: {
+//           _id: 2,
+//           name: "React Native",
+//           avatar: "https://placeimg.com/140/140/any",
+//         },
+//       },
+//       {
+//         _id: 2,
+//         text: "This is a system message",
+//         createdAt: new Date(),
+//         system: true,
+//       },
+//     ]);
+//   }, []);
 
-  // checking for updates in collection
-  useEffect(() => {
-    unsubscribe = referenceMessages.onSnapshot(onCollectionUpdate);
-    return () => unsubscribe();
-  }, []);
+//   // checking for updates in collection
+//   useEffect(() => {
+//     unsubscribe = referenceMessages.onSnapshot(onCollectionUpdate);
+//     return () => unsubscribe();
+//   }, []);
 
-  //
-  useEffect(()=> {
-    const authUnsubscribe = firebase.auth().onAuthStateChanged((user)=> {
-      if (!user) {
-        firebase.auth().signInAnonymously();
-      }
-      setUid({uid})
-      setMessages({messages})
-    });
-    unsubscribe = referenceMessages.orderBy('createdAt', 'desc').onSnapshot(onCollectionUpdate);
-  }, [])
+//   //
+//   useEffect(()=> {
+//     const authUnsubscribe = firebase.auth().onAuthStateChanged((user)=> {
+//       if (!user) {
+//         firebase.auth().signInAnonymously();
+//       }
+//       setUid({uid})
+//       setMessages({messages})
+//     });
+//     unsubscribe = referenceMessages.orderBy('createdAt', 'desc').onSnapshot(onCollectionUpdate);
+//   }, [])
 
-  // function to retrieve data from a collection
-  function onCollectionUpdate(querySnapshot) {
-    const messages = [];
-    // go through each document
-    querySnapshot.forEach((doc) => {
-      // get the queryDocumnetSnapshot's data
-      let data = doc.data();
-      messages.push({
-        _id: data._id,
-        text: data.text,
-        createdAt: data.createdAt,
-        user: data.user,
-      });
-    });
-  }
+//   // function to retrieve data from a collection
+//   function onCollectionUpdate(querySnapshot) {
+//     const messages = [];
+//     // go through each document
+//     querySnapshot.forEach((doc) => {
+//       // get the queryDocumnetSnapshot's data
+//       let data = doc.data();
+//       messages.push({
+//         _id: data._id,
+//         text: data.text,
+//         createdAt: data.createdAt,
+//         user: data.user,
+//       });
+//     });
+//   }
 
-  // add message to collection
-  function addMessages(message) {
-    referenceMessages.add({
-      _id: message._id,
-      text: message.text || "",
-      createdAt: message.createdAt,
-      user: message.user,
-      uid: message.uid
-    });
-  }
+//   // add message to collection
+//   function addMessages(message) {
+//     referenceMessages.add({
+//       _id: message._id,
+//       text: message.text || "",
+//       createdAt: message.createdAt,
+//       user: message.user,
+//       uid: message.uid
+//     });
+//   }
 
-  // function to send message in chat room
-  function onSend(messages = []) {
-    setMessages((prevState) => GiftedChat.append(prevState.messages, messages));
-    addMessages(messages[0]);
-  }
+//   // function to send message in chat room
+//   function onSend(messages = []) {
+//     setMessages((prevState) => GiftedChat.append(prevState.messages, messages));
+//     addMessages(messages[0]);
+//   }
 
-  // function to alter the bakcgorund color of bubble when user sends msg in chat
-  function renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          // left: {
-          //   backgroundColor: 'blue'
-          // },
-          right: {
-            backgroundColor: "#242ACF",
-          },
-        }}
-      />
-    );
-  }
+//   // function to alter the bakcgorund color of bubble when user sends msg in chat
+//   function renderBubble(props) {
+//     return (
+//       <Bubble
+//         {...props}
+//         wrapperStyle={{
+//           // left: {
+//           //   backgroundColor: 'blue'
+//           // },
+//           right: {
+//             backgroundColor: "#242ACF",
+//           },
+//         }}
+//       />
+//     );
+//   }
 
-  return (
-    <View style={[{ backgroundColor: color }, styles.chatContainer]}>
-      {/* <Text style={styles.txt}>Hello {name} from Chat!</Text> */}
-      <Text>{loggedText}</Text>
-      <GiftedChat
-        renderBubble={renderBubble}
-        messages={messages}
-        onSend={(messages) => {
-          console.log(messages, "---------");
-           onSend(messages)
-        }}
+//   return (
+//     <View style={[{ backgroundColor: color }, styles.chatContainer]}>
+//       {/* <Text style={styles.txt}>Hello {name} from Chat!</Text> */}
+//       <Text>{loggedText}</Text>
+//       <GiftedChat
+//         renderBubble={renderBubble}
+//         messages={messages}
+//         onSend={(messages) => {
+//           console.log(messages, "---------");
+//            onSend(messages)
+//         }}
         
-        user={{ _id: 1 }}
-      />
-      {/* this prevents the screen collapsing with a keyboard */}
-      {Platform.OS === "android" ? (
-        <KeyboardAvoidingView behavior="height" />
-      ) : null}
-    </View>
-  );
-}
+//         user={{ _id: 1 }}
+//       />
+//       {/* this prevents the screen collapsing with a keyboard */}
+//       {Platform.OS === "android" ? (
+//         <KeyboardAvoidingView behavior="height" />
+//       ) : null}
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  chatContainer: {
-    flex: 1,
-    justifyContent: "center",
-  },
+// const styles = StyleSheet.create({
+//   chatContainer: {
+//     flex: 1,
+//     justifyContent: "center",
+//   },
 
-  txt: {
-    fontSize: 16,
-    color: "#000",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-  },
-});
+//   txt: {
+//     fontSize: 16,
+//     color: "#000",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: 10,
+//   },
+// });
